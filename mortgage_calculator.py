c940d229070e91554c89ac437a492154570bb250
Adewale Maleek Ayobami <adewalemaleekayobami2528@gmail.com>
2024-08-26 08:44:29 +0100

    first commit

---
+++ mortgage_calculator.py
@@ -0,0 +1,40 @@
+"""
+import math to able to access the method math.pow
+prompt the user to enter the principal amount and save as principal
+prompt the user to enter the annual interest rate and save as annual interest rate
+prompt the user to enter the duration in years and save as duration in years
+multiply the duration in years by number of months which is 12 and save it in a variable called duration
+divide the annual interest rate by percentage and save in a variable called annual interest
+divide the annual interest rate by number of months which is 12 and save in a variable called monthly interest rate
+add 1 plus the monthly interest rate and save in a variable called monthly payment 1
+do monthly payment 1 to be raise to power duration and save in a variable called monthly payment 2
+multiply monthly interest rate and monthly payment 2 and save in a variable called monthly payment 3
+do the difference between monthly payment 2 and 1 and save in a variable called monthly payment 4
+divide monthly payment 3 by monthly payment 4 and save in a variable called division result
+multiply the principal by division result and save in a variable called multiplication
+print out the result
+
+
+"""
+
+
+import math
+
+principal = float(input("Enter the Principal Amount: "))
+annual_interest_rate = float(input("Enter the Annual Interest Rate: "))
+duration_in_years = float(input("Enter the Duration In years: "))
+
+
+duration = duration_in_years * 12
+annual_interest =  annual_interest_rate / 100
+monthly_interest_rate = annual_interest /12
+
+monthly_payment_1 = (1 + monthly_interest_rate)
+monthly_payment_2 = math.pow(monthly_payment_1, duration)
+monthly_payment_3 = monthly_interest_rate * monthly_payment_2
+
+monthly_payment_4 = monthly_payment_2 - 1
+division_result = monthly_payment_3 / monthly_payment_4
+multiplication = principal * division_result 
+
+print("Your monthly payment is &",(multiplication))
---
+++ nokia3310.py
@@ -0,0 +1,302 @@
+menu = '''
+
+Welcome to Nokia 3310 !
+
+please select the menus below to continue:
+------------------------------------------
+	1 >>> Phone Book
+	2 >>> Message
+	3 >>> Chat
+	4 >>> Call Register
+	5 >>> Tones
+	6 >>> Settings
+	7 >>> Call Divert
+	8 >>> Games
+	9 >>> Calculator 
+	10 >>> Remainder
+	11 >>> Clock
+	12 >>> Profiles
+	13 >>> SIM Services
+------------------------------------------
+
+'''
+print(menu)
+
+phone_book = int(input("Enter from 1 to 13:"))
+
+match phone_book: 
+	case 1: 
+		phone_book_menu = '''
+	
+
+	Welcome to the Phone Book Menu !
+	------------------------------------------
+	1 >>> Search
+	2 >>> Service Nos
+	3 >>> Add Name
+	4 >>> Erase
+	5 >>> Edit
+	6 >>> Assign Tone
+	7 >>> Send b'card
+	8 >>> Options
+	9 >>> Speed Dials
+	10 >>> Voice Tags
+	------------------------------------------
+	'''
+		print(phone_book_menu)
+	case 2: 
+		message = '''Welcome to Messages
+
+	Please select the menus below to continue:
+	------------------------------------------
+	1 >>> Write Messages
+	2 >>> Inboxq
+	3 >>> Outbox
+	4 >>> Picture Messages
+	5 >>> Templates
+	6 >>> Smileys
+	7 >>> Message Settings
+	8 >>> Info Service
+	9 >>> Voice Mailbox Number
+	10 >>> Service Command Editor
+	------------------------------------------
+	'''
+
+		print(message)
+	case 3: 
+		chat = '''Welcome to chat
+
+	Please select the menus below to continue:
+	------------------------------------------
+	1 >>> Chat
+	------------------------------------------
+	'''
+		print(chat)
+	case 4: 
+		call_register = '''Welcome to Call Register
+
+	Please select the menus below to continue:
+	------------------------------------------
+	1 >>> Missed Calls
+	2 >>> Recieved Calls
+	3 >>> Dialled Numbers
+	4 >>> Erase Recent Call Lists
+	5 >>> Show Call Duration
+	6 >>> Show Call Cost
+	7 >>> Call Cost Settings
+	8 >>> Prepaid Credit
+	------------------------------------------
+	'''
+		print(call_register)
+
+	
+	case 5: 
+		tones = '''
+	Welcome to Tones
+
+	Please select the menus below to continue:
+	------------------------------------------
+	1 >>> Ringing Tone
+	2 >>> Ringing Volume
+	3 >>> Incoming Call Alert
+	4 >>> Composer
+	5 >>> Message Alert Tone
+	6 >>> Keypad Tones
+	7 >>> Warning And Game Tones
+	8 >>> Vibrating Alert
+	8 >>> Screen Saver
+	------------------------------------------
+	''' 
+		print(tones)
+
+	
+	case 6: 
+		settings =  '''Welcome to Settings
+
+	Please select the menus below to continue:
+	------------------------------------------
+	1 >>> Call Settings
+	2 >>> Phone Settings
+	3 >>> Security Settings
+	4 >>> Restore Factory Settings
+	------------------------------------------
+	''' 
+		print(settings)
+
+
+	
+	case 7: 
+		call_divert = '''Welcome to Call Divert
+
+	Please select the menus below to continue:
+	------------------------------------------
+	1 >>> Call Divert
+ 	------------------------------------------
+	'''
+
+		print(call_divert)
+
+	
+	case 8: 
+		games = '''
+	Welcome to Games
+
+	Please select the menus below to continue:
+	------------------------------------------
+	1 >>> Games
+ 	------------------------------------------
+	'''
+		print(games)
+
+
+	
+	case 9: 
+		calculator = '''
+	Welcome to Calculator
+
+	Please select the menus below to continue:
+	------------------------------------------
+	1 >>> Calculator
+ 	------------------------------------------
+	'''
+		print(calculator)
+
+	
+	case 10: 
+		remainder = '''
+	Welcome to Remainders
+
+	Please select the menus below to continue:
+	------------------------------------------
+	1 >>> Remainders
+ 	------------------------------------------
+	'''
+		print(ramainder)
+
+	
+	case 11: 
+		clock = '''Welcome to Clock
+
+	Please select the menus below to continue:
+	------------------------------------------
+	1 >>> Clock
+ 	------------------------------------------
+	''' 
+		print(clock)
+
+	
+	case 12: 
+		profiles = '''Welcome to Profiles
+
+	Please select the menus below to continue:
+	------------------------------------------
+	1 >>> Profiles
+ 	------------------------------------------
+	'''
+		print(profiles)
+
+	
+	case 13: 
+		sim_service = '''Welcome to SIM Service
+
+	Please select the menus below to continue:
+	------------------------------------------
+	1 >>> SIM Service
+ 	------------------------------------------
+	'''
+
+		print(sim_service)
+
+
+		
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
