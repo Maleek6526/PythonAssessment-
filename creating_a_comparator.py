764da45a591375b8da4de87e1ddc76bd9c06a7b6
Adewale Maleek Ayobami <adewalemaleekayobami2528@gmail.com>
2024-09-04 21:16:53 +0100

    Creating a Comparator

---
+++ creating_a_comparator.py
@@ -0,0 +1,14 @@
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+
+if first_number == second_number:
+		
+	print("0")
+
+if first_number > second_number:
+
+	print("1")
+
+if first_number < second_number:
+
+	print("-1")
