a0467f92944cf458c7c4816a76fb300e820376de
Adewale Maleek Ayobami <adewalemaleekayobami2528@gmail.com>
2024-09-18 08:07:48 +0100

    Task 8

--- how far i have gone/task8.py
+++ how far i have gone/task8.py
@@ -2,12 +2,19 @@ count = 0
 counter = 0
 while True:
 	user_input = int(input("Enter score: "))
-	if user_input >= 0 or user_input <= 100:
-		count+=1
+	if user_input >= 0 and user_input <= 100:
 		counter += user_input
+		count += 1
 
 	if count == 10:
 		print(counter) 
 		break
 
+
+
+
+
+		
+		
+	
 
