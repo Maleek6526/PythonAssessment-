64a54a833b82ee1b325da30d72c42edbfd44ca4f
DELL <nwangwudinma22@gmail.com>
2024-10-04 13:28:13 +0100

    reverse_palindrome

--- Phase Gate Two/reverse_palindrome.py
+++ Phase Gate Two/reverse_palindrome.py
@@ -1,3 +1,9 @@
+'''
+1. write two functions 
+2. the fist function should take reverse, while the second takes palindrome
+3. The first function returns a reverse number
+4. The second function checks maybe the number is a palindrome or not
+'''
 def reverse(numbers):
 	reverse_number = ''
 	for number in range(len(numbers)-1, -1, -1):
