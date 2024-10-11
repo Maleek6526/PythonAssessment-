ba5833b72b608e61896bd4bd3735e3dd582aa41a
DELL <nwangwudinma22@gmail.com>
2024-10-11 15:16:01 +0100

    fizz_buzz.py

--- Phase Gate Three/Phase Gate 4/fizz_buzz.py
+++ Phase Gate Three/Phase Gate 4/fizz_buzz.py
@@ -1,15 +1,9 @@
 for counter in range(1, 51):
-	displayer = ""
-	if counter % 3 == 0:
-		displayer = "fizz"
-		print(displayer, end = ' ')
-		continue
-	if counter % 5 == 0:
-		displayer = "buzz"
-		print(displayer, end = ' ')
-		continue
 	if counter % 3 == 0 and counter % 5 == 0:
-		displayer = "FizzBuzz"
-		print(displayer, end = ' ')
-		continue
-	print(counter, end = ' ')
+		print("fizzbuzz")
+	elif counter % 3 == 0:
+		print("fizz")
+	elif counter % 5 == 0: 
+		print("buzz")
+	else: 
+		print(counter)
