d7a97fe125a5ac7557cb08c7d43defbebc54f48b
DELL <nwangwudinma22@gmail.com>
2024-10-11 12:42:07 +0100

    investment_rate.py

--- Phase Gate Three/Phase Gate 4/investment_rate.py
+++ Phase Gate Three/Phase Gate 4/investment_rate.py
@@ -1,16 +1,18 @@
-investment_amount = float(input("Enter the investment amount: "))
-number_of_years = int(input("Enter the number of years: "))
-percentage = int(input("Enter the percentage: "))
+try:
+	investment_amount = float(input("Enter the investment amount: "))
+	number_of_years = int(input("Enter the number of years: "))
+	percentage = int(input("Enter the percentage: "))
 
-print(f"Years \t Interest \t Amount")
+	if investment_amount > 0 or number_of_years > 0 or percentage > 0:
+		print(f"Years \t Interest \t Amount")
+		for count in range(1, number_of_years+1):
+			interest_rate = (10/percentage) * investment_amount
+			balance = investment_amount + interest_rate
+			print(f"{count} \t {interest_rate:,.2f} \t {balance:,.2f}")
+			investment_amount = balance
 
+	else:
+		print("Please input a valid amount!")
 
-
-for count in range(1, number_of_years+1):
-
-	interest_rate = (10/percentage) * investment_amount
-	balance = investment_amount + interest_rate
-
-	print(f"{count}\t {interest_rate:,.2f}\t{balance:,.2f}")
-
-	investment_amount = balance
+except ValueError as e:
+	print(f"Caught an exception: {e}")
