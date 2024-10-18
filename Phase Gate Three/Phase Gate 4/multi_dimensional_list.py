98eea4f6ee2a2b14eb9755d20693473a171d13c2
Adewale Maleek Ayobami <adewalemaleekayobami2528@gmail.com>
2024-10-18 14:59:45 +0100

    multi_dimensional_list.py

---
+++ Phase Gate Three/Phase Gate 4/multi_dimensional_list.py
@@ -0,0 +1,9 @@
+userInput = int(input("Enter the number of rows: "))
+userInput2 = int(input("Enter the number of column: "))
+list = [[0] * userInput] * userInput2
+
+for row in range(len(list)):
+	for column in range(len(list[row])):
+		list[row][column] = row * column
+		print(list[row][column], end = ' ')
+	print()
