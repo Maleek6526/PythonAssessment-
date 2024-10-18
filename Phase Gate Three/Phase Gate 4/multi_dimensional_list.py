822aa0e6e2068265f808b07c442485a3da118893
Adewale Maleek Ayobami <adewalemaleekayobami2528@gmail.com>
2024-10-18 15:36:45 +0100

    multi_dimensional_list.py

--- Phase Gate Three/Phase Gate 4/multi_dimensional_list.py
+++ Phase Gate Three/Phase Gate 4/multi_dimensional_list.py
@@ -1,9 +1,17 @@
-userInput = int(input("Enter the number of rows: "))
-userInput2 = int(input("Enter the number of column: "))
-list = [[0] * userInput] * userInput2
+try:
+	userInput = int(input("Enter the number of rows: "))
+	userInput2 = int(input("Enter the number of column: "))
 
-for row in range(len(list)):
-	for column in range(len(list[row])):
-		list[row][column] = row * column
-		print(list[row][column], end = ' ')
-	print()
+	if userInput > 0 and userInput2 > 0:
+		list = [[0] * userInput] * userInput2
+
+		for row in range(len(list)):
+			for column in range(len(list[row])):
+				list[row][column] = row * column
+				print(f"{list[row][column]} \t", end = ' ')
+			print()
+
+	else:
+		print("Input a valid number")
+except:
+	print("Invalid input")
